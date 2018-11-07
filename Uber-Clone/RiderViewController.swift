a270f94ffacfffece2bdeb1be61e191c27be44b8
Koceto1973 <koceto1973@yahoo.ca>
2018-11-07 02:18:11 -0800

    refactor .1

--- Uber-Clone/RiderViewController.swift
+++ Uber-Clone/RiderViewController.swift
@@ -15,17 +15,20 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     
     // Outlets
     @IBOutlet weak var map: MKMapView!
-    @IBOutlet weak var callAnUberButton: UIButton!
+    @IBOutlet weak var callDriver: UIButton!
     
     // variables
     var locationManager = CLLocationManager()
-    var userLocation = CLLocationCoordinate2D()
-    var uberHasBeenCalled = false
-    var driverOnTheWay = false
+    var riderLocation = CLLocationCoordinate2D()
     var driverLocation = CLLocationCoordinate2D()
-
+    var driverCalled = false
+    var driverAccepted = false
+    var dbRef : DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dbRef = Database.database().reference()
 
         // Do any additional setup after loading the view.
         locationManager.delegate = self
@@ -34,25 +37,25 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         
         if let email = Auth.auth().currentUser?.email {
-            Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded, with: { (snapshot) in
-                self.uberHasBeenCalled = true
-                self.callAnUberButton.setTitle("Cancel Uber", for: .normal)
-                Database.database().reference().child("RideRequests").removeAllObservers()
+            dbRef.child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded, with: { (snapshot) in
+                self.driverCalled = true
+                self.callDriver.setTitle("Cancel Uber", for: .normal)
+                self.dbRef.child("RideRequests").removeAllObservers()
                 
                 if let rideRequestDictionary = snapshot.value as? [String:AnyObject] {
                     if let driverLat = rideRequestDictionary["driverLat"] as? Double {
                         if let driverLon = rideRequestDictionary["driverLon"] as? Double {
                             self.driverLocation = CLLocationCoordinate2D(latitude: driverLat, longitude: driverLon)
                             
-                            self.driverOnTheWay = true
+                            self.driverAccepted = true
                             self.displayDriverAndRider()
                             
-                            if let email = Auth.auth().currentUser?.email{ Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childChanged, with: { (snapshot) in
+                            if let email = Auth.auth().currentUser?.email{ self.dbRef.child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childChanged, with: { (snapshot) in
                                     if let rideRequestDictionary = snapshot.value as? [String:AnyObject] {
                                         if let driverLat = rideRequestDictionary["driverLat"] as? Double {
                                             if let driverLon = rideRequestDictionary["driverLon"] as? Double {
                                                 self.driverLocation = CLLocationCoordinate2D(latitude: driverLat, longitude: driverLon)
-                                                self.driverOnTheWay = true
+                                                self.driverAccepted = true
                                                 self.displayDriverAndRider()
                                             }
                                         }
@@ -68,20 +71,20 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     
     func displayDriverAndRider() {
         let driverCLLocation = CLLocation(latitude: driverLocation.latitude, longitude: driverLocation.longitude)
-        let riderCLLocation = CLLocation(latitude: userLocation.latitude, longitude: userLocation.longitude)
+        let riderCLLocation = CLLocation(latitude: riderLocation.latitude, longitude: riderLocation.longitude)
         let distance = driverCLLocation.distance(from: riderCLLocation) / 1000
         let roundedDistance = round(distance * 100) / 100
-        callAnUberButton.setTitle("Your driver is \(roundedDistance)km away!", for: .normal)
+        callDriver.setTitle("Your driver is \(roundedDistance)km away!", for: .normal)
         map.removeAnnotations(map.annotations)
         
-        let latDelta = abs(driverLocation.latitude - userLocation.latitude) * 2 + 0.005
-        let lonDelta = abs(driverLocation.longitude - userLocation.longitude) * 2 + 0.005
+        let latDelta = abs(driverLocation.latitude - riderLocation.latitude) * 2 + 0.005
+        let lonDelta = abs(driverLocation.longitude - riderLocation.longitude) * 2 + 0.005
         
-        let region = MKCoordinateRegion(center: userLocation, span: MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta))
+        let region = MKCoordinateRegion(center: riderLocation, span: MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta))
         map.setRegion(region, animated: true)
         
         let riderAnno = MKPointAnnotation()
-        riderAnno.coordinate = userLocation
+        riderAnno.coordinate = riderLocation
         riderAnno.title = "Your Location"
         map.addAnnotation(riderAnno)
         
@@ -94,9 +97,9 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let coord = manager.location?.coordinate {
             let center = CLLocationCoordinate2D(latitude: coord.latitude, longitude: coord.longitude)
-            userLocation = center
+            riderLocation = center
             
-            if uberHasBeenCalled {
+            if driverCalled {
                 displayDriverAndRider()
             } else {
                 let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
@@ -112,31 +115,29 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
-    @IBAction func LogOutPressed(_ sender: Any) {
-        try? Auth.auth().signOut()
-        navigationController?.dismiss(animated: true, completion: nil)
-    }
-
     @IBAction func callUberPressed(_ sender: Any) {
         if let email = Auth.auth().currentUser?.email {
-            
-            if uberHasBeenCalled {
-                uberHasBeenCalled = false
-                callAnUberButton.setTitle("Call an Uber", for: .normal)
-                Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded, with: { (snapshot) in
+            if driverCalled {
+                driverCalled = false
+                callDriver.setTitle("Call an Uber", for: .normal)
+                
+                dbRef.child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded, with: { (snapshot) in
                     snapshot.ref.removeValue()
                     Database.database().reference().child("RideRequests").removeAllObservers()
                 })
-            } else {
-                let rideRequestDictionary : [String:Any] = ["email":email,"lat":userLocation.latitude,"lon":userLocation.longitude]
-                Database.database().reference().child("RideRequests").childByAutoId().setValue(rideRequestDictionary)
-                uberHasBeenCalled = true
-                callAnUberButton.setTitle("Cancel Uber", for: .normal)
+            } else {  // driver call not yet placed
+                driverCalled = true
+                callDriver.setTitle("Cancel Uber", for: .normal)
+                
+                let rideRequestDictionary : [String:Any] = ["email":email,"lat":riderLocation.latitude,"lon":riderLocation.longitude]
+                dbRef.child("RideRequests").childByAutoId().setValue(rideRequestDictionary)
             }
-            
-            
         }
     }
     
-
+    @IBAction func LogOutPressed(_ sender: Any) {
+        try? Auth.auth().signOut()
+        debugPrint("Log Out success")
+        navigationController?.dismiss(animated: true, completion: nil)
+    }
 }
